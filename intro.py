6da653ee37931566bfa62ea4f5ea9413933bc9d9
nekoo <nereapena42gmail.com>
2023-03-09 22:30:24 +0100

    dibujando figuras en la pantalla

--- intro.py
+++ intro.py
@@ -1,7 +1,15 @@
-import pygame, sys
+import pygame
+import sys
 
 # incializando libreria
 pygame.init()
+
+# definiendo colores para dibujar
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 # creamos ventana
 size = (800, 500)
@@ -12,3 +20,15 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # si le damos a la X, sale del juego
             sys.exit()
+
+    # color que le ponemos a la pantalla
+    screen.fill(WHITE)
+    # dibujo de pantalla
+
+    pygame.draw.line(screen, GREEN, [0, 100], [200, 300], 5)
+    # dibujando un rectangulo
+    pygame.draw.rect(screen, BLACK, (100, 100, 80, 80))
+    # dibujando un circulo
+    pygame.draw.circle(screen, BLACK, (200, 200), 30)
+    # actualizar la pantalla con los cambios
+    pygame.display.flip()
